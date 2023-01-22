4aa108f6c84731685e9b105ec8e7dc0c103c6248
Megumi Sugita <megumisugita0615@gmail.com>
2023-01-23 00:25:24 +0900

    モデル、マイグレーション、シーダーを実装（

---
+++ backend/database/seeders/ArticleSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ArticleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('articles')->insert([
+            'title' => Str::random(10),
+            'body' => Str::random(100)
+        ]);
+    }
+}
