624d5211be71294074e1d5c1a8cbef6aed6cddee
Megumi Sugita <megumisugita0615@gmail.com>
2023-01-23 19:46:32 +0900

    install sanctum

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -39,7 +39,8 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            // SPA認証
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
